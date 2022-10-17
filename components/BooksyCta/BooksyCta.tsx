af75961aa0887127befa9819cdb234c69f791aa8
Armin Hupka <arminhupka@me.com>
2022-10-17 13:51:10 +0200

    -udpated booksy cta image margin -added link to booksy

--- components/BooksyCta/BooksyCta.tsx
+++ components/BooksyCta/BooksyCta.tsx
@@ -20,19 +20,21 @@ const TWGrid = tw.div<any>`
     md:grid-cols-2
 `
 
-const TWGridItem = tw.div<any>``
+const TWGridItem = tw.div<any>`
+    relative
+`
 
 
 const BooksyCta = (): ReactElement => (
     <TWWrapper>
         <TWContainer>
             <TWGrid>
-                <TWGridItem className="space-y-4 flex flex-col justify-center items-start">
+                <TWGridItem className="space-y-4 flex flex-col justify-center items-start mb-4">
                     <h2 className="text-3xl font-semi font-prata">Znajdź nas na Booksy</h2>
                     <p>Już teraz możesz nas znaleźć w serwisie Booksy dzięki któremu możesz znaleźć odpowiedni dla siebie termin na skorzystanie oraz otrzymywać przypomnienia o umówionej wizycie.</p>
-                    <TWLinkButton>Umów wizytę na Booksy</TWLinkButton>
+                    <TWLinkButton href="https://booksy.com/pl-pl/162508_margarida-salon_fryzjer_15108_kobierzyce">Umów wizytę na Booksy</TWLinkButton>
                 </TWGridItem>
-                <TWGridItem>
+                <TWGridItem className="mt-16 lg:mt-0">
                     <Image src="/img/girl.png" layout="responsive" width={250} height={200} objectFit="contain"/>
                 </TWGridItem>
             </TWGrid>
